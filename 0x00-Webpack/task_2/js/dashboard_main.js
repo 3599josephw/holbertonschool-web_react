61b544cdcfb88b671ae4ad824e1cd65007f1885d
3599josephw <3599@holbertonschool.com>
2023-01-10 12:53:22 -0600

    task 2

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,24 @@
+import $ from "jquery";
+import "./styles.css"
+const _ = require('lodash');
+
+
+$('body').append("\
+  <p>Holberton Dashboard</p>\
+  <p>Dashboard data for the students</p>");
+
+$('body').append("<button>Click here to get started</button>");
+
+$('body').append("<p id='count'></p>");
+
+$('body').append("<p>Copyright - Holberton School</p>");
+
+
+let count = 0;
+
+$('#count').on("click", function updateCounter() {
+  click += 1;
+  $('#count').text(`${count} clicks on the button`);
+});
+
+const debounced = _.debounce(updateCounter, 500);
