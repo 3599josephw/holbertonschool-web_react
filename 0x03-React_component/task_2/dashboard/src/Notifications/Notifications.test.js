51ef4eaadeecb0a12ad5028f6926993710c4b29c
3599josephw <3599@holbertonschool.com>
2023-01-17 11:52:56 -0600

    task_2

---
+++ 0x03-React_component/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Notifications from './Notifications';
+import { shallow } from 'enzyme';
+import { assert } from 'chai';
+
+
+describe('Notifications tests', () => {
+  describe('Notifications renders without crashing', () => {
+    it('Renders', function() {
+      shallow(<Notifications />);
+    });
+  });
+  describe('Notifications renders three list items', () => {
+    it('Renders', function() {
+      const wrapper = shallow(<Notifications />);
+      const list = wrapper.find('li');
+      assert.equal(list.length, 3);
+    });
+  });
+  describe('Notifications renders "Here is the list of notifications"', () => {
+    it('Renders', function() {
+      const wrapper = shallow(<Notifications />);
+      const text = wrapper.find('p');
+      expect(text.text()).toEqual('Here is the list of notifications');
+    });
+  });
+
+  it('console logs id when clicked', () => {
+    jest.spyOn(console, 'log').mockImplementation(() => {});
+    const wrapper = shallow(
+      <Notifications displayDrawer listNotifications={listNotifications} />
+    );
+    wrapper.instance().markAsRead(1);
+    expect(console.log).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith(
+      'Notification 1 has been marked as read'
+    );
+    console.log.mockRestore();
+  });
+});
