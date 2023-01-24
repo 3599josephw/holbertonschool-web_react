6866bc9fb3d8d8392cfd5951b7843aaea82f1df8
3599josephw <3599@holbertonschool.com>
2023-01-24 11:59:30 -0600

    task 2

---
+++ 0x00-react_state/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,49 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+
+const styles = StyleSheet.create({
+  default: {
+    color: '#1D0563',
+    borderBottom: '2px solid black',
+    marginBottom: '10px',
+    paddingBottom: '5px',
+    listStyleType: 'none',
+  },
+  urgent: {
+    color: 'red',
+    borderBottom: '2px solid black',
+    marginBottom: '10px',
+    paddingBottom: '5px',
+    listStyleType: 'none',
+  },
+});
+
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  value: PropTypes.string,
+  markAsRead: PropTypes.func.isRequired
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+};
+
+function NotificationItem({type, html, value, markAsRead}) {
+  if (!value) {
+    return (
+      <li className={css(type === 'default' ? styles.default : styles.urgent)} data-priority={type} dangerouslySetInnerHTML={html} onClick={markAsRead}></li>
+    );
+  }
+  return (
+    <li className={css(type === 'default' ? styles.default : styles.urgent)} data-priority={type} onClick={markAsRead}>
+      {value}
+    </li>
+  );
+}
+
+export default memo(NotificationItem);
